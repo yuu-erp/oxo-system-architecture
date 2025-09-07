4b5031a9b719b742fab853d1f596a344c2b0cb6d
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2025-09-07 12:01:18 +0700

    refactor(backend): setup package.json version pnpm 🐳

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,21 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "import"],
-  extends: ["eslint:recommended","plugin:@typescript-eslint/recommended","plugin:import/recommended","plugin:import/typescript","prettier"],
-  rules: { "import/order": ["error", { "newlines-between": "always" }] },
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project: ["./tsconfig.base.json"],
-      },
-    },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.base.json'],
   },
-  ignorePatterns: ["dist", "node_modules"]
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/order': ['error', { 'newlines-between': 'always' }],
+  },
+  ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules'],
 };
